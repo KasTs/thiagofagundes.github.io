f6ca5995070d2839453b2bb2a1fce42f435f2113
kasts <kasfagundes@gmail.com>
2022-06-19 09:15:11 -0300

    FIX: correct for and add view CARROUSEL_Types

--- assets/js/scritps.js
+++ assets/js/scritps.js
@@ -3,20 +3,25 @@ $(function() {
     const url = "https://graph.instagram.com/me/media?access_token="+token+"&fields=media_url,media_type,caption,permalink";
 
     $.get(url).then(function(response){
-        // console.log('retorno: ', response.data)
+        console.log('retorno: ', response.data)
         let dadosJson = response.data
         let conteudo ='<div class="row" style="padding-left:5px">';
         let p = 0;
-        for (p,  p < dadosJson.length; p++;){
+        for (p;  p < dadosJson.length; p++){
             let feed    = dadosJson[p];
             let titulo  = feed.caption !== null ? feed.caption : '';
             let tipo    = feed.media_type;
+
             if(tipo === 'VIDEO') { 
-                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-x1-4 col-xxl-4"><video style="width:100%; heigth:90%" controls><source src="'+feed.media_url+'"type="video/mp4" </video> </div>';
+                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-x1-4 col-xxl-4"><video style="width:100%; heigth:90%; cursor:pointer" controls><source src="'+feed.media_url+'"type="video/mp4" </video> </div>';
+            }
+            else if(tipo === 'CAROUSEL_ALBUM'){
+                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-x1-4 col-xxl-4"><img style="width:100%; heigth:90%; cursor:pointer" title="'+titulo+'" alt="'+titulo+'" src= "'+feed.media_url+'" onclick="window.open(\'' + feed.permalink + '\');"> </div>';
             }
             else if(tipo === 'IMAGE'){
-                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-x1-4 col-xxl-4"><img style="width:100%; heigth:90%" title="'+titulo+'" alt="'+titulo+'" src= "'+feed.media_url+'" onclick="window.open(\'' + feed.permalink + '\');"> </div>';
+                conteudo += '<div class="col-12 col-sm-6 col-md-4 col-lg-4 col-x1-4 col-xxl-4"><img style="width:100%; heigth:90%; cursor:pointer" title="'+titulo+'" alt="'+titulo+'" src= "'+feed.media_url+'" onclick="window.open(\'' + feed.permalink + '\');"> </div>';
             }
+
 
     
 
